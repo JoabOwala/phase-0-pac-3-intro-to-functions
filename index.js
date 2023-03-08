e9d2009efa561f6b31b373397e97c977dcadce11
Joab Owala <owalajoab@gmail.com>
2023-03-08 13:12:06 +0300

    Coding along

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Follow along with the examples here
+// function sayHelloToIsabel() {
+//     console.log("Hello, Isabel!");
+// }
+// function sayHelloToSofia() {
+//   console.log("Hello, Sofia!");
+// }
+// function sayHelloToBrendan() {
+//   console.log("Hello, Brendan!");
+// }
+// sayHelloToIsabel();
+// sayHelloToSofia();
+// sayHelloToBrendan();
+
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}!`);
+// }
+
+// sayHelloTo("Isabel");
+// sayHelloTo("Jane");
+// sayHelloTo("R2-D2");
+// sayHelloTo(1);
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+
+// say("Julio", "Hello");
+
+// function add(x,y) {
+//     return x + y;
+// }
+// console.log(add(1, 2));
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+
+// console.log(say("Hello", "Sofia"));
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+}
+
+console.log(say("Hello","R2-D2"));
